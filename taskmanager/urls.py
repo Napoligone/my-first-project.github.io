466b340bf4ba5180bb1e4151e889511cf8b09017
Napoligone <156519524+Napoligone@users.noreply.github.com>
2024-01-13 16:47:03 +0300

    Add files via upload

---
+++ taskmanager/urls.py
@@ -0,0 +1,8 @@
+# Это основной ЮРЛ файл
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include('main.urls'))
+]
